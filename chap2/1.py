53b8eb6a23bbd78666697ac137eaedb77db45339
정은영 <eeee@naver.com>
2023-03-21 12:40:54 +0900

    asdf

---
+++ chap2/1.py
@@ -0,0 +1,18 @@
+#1.py
+print("hello world")
+
+print("hello 'world'")
+print(3)
+print(type(1.1))
+
+print(1,2,-5,3.14,2.6191)
+print('hi','python')
+print('23000 원은', '500원 ?개','1000원 ?개')
+print('5000원', 23000 // 5000, '개')
+print('1000원',(23000%5000)//1000,'개')
+
+eval('3+15/2')
+eval('4*3%5')
+eval('3* -2**3')
+# eval("java" * 3)
+
