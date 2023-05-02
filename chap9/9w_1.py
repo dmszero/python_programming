3773f61206f8475f44593d42b36df2870cb36a84
dmszero <dmszero89@gamil.com>
2023-05-02 12:55:43 +0900

    chap9 function

--- chap9/9w_1.py
+++ chap9/9w_1.py
@@ -8,6 +8,7 @@ output = 숫자 -output_num
 
 #funtion 
 # Ex1) 3을 곱하는 함수
+'''
 def multi (num1) : # 들여쓰기 주의
     output_num = num1 *3
     print("여기는 multi 함수 안입니다.")
@@ -64,3 +65,83 @@ def addone():
    
 addone()
 print("***num: ", num)
+
+#======= 인자 갯수 여러개 ---> * 사용 =======
+# Ex4)
+#인자의 갯수가 여러 개인 경우
+print()
+print(1,2,3,4,5,6,7,8,9)
+print()
+def hello2(*names):
+    for i in names:
+        print("hello,",i) #hello홍길동
+hello2("홍길동","김길동","고길동")
+
+#Ex5)
+# 여러 개 인자의 합을 구하는 함수
+def Sum(*numbers): # *가 있으면 갯수 상관없이
+    result =0
+    for i in numbers:
+        result = result+i
+    return result
+print(Sum(1,2,3,4,5))
+l1=[1,2,3,4,5,6,7,8,9,10]
+print(Sum(*l1))
+
+
+#딕셔너리를 이용한 함수
+#dictionary = {key1:value,key2:value2} 
+coffee ={"americano":2000,"latte":3000,"tea":4000} 
+def printmenu(**keyvalue):
+    for k in keyvalue:
+        print(k,keyvalue[k])
+
+printmenu(**coffee)
+printmenu(hotchoco=3000,pizza=6000,coke=2000)
+
+def fun1(*num,**menu):
+    #num들의 합
+    result1 = 0
+    for i in num:
+        result = result +i
+    return result
+    # menu출력
+    for k in menu:
+        print(k,menu[k])
+l1=[1,2,3,4,5,6,7,8,9,10]
+coffee ={"americano":2000,"latte":3000,"tea":4000} 
+
+fun1(*l1,*coffee)
+'''
+
+#========== lambda function ============
+#function을 만드는데, 이름짓기가 귀찮다. 실행문이 하나 밖에 없다
+def addone(x):
+    return x +1
+print(addone(100))
+
+#lambda parameter_name : parameter로 실행하는 문
+print((lambda x:x+1)(100)) # addone(100) = (100)--> 두 개 똑같음
+
+def mysum2(num1,num2):
+    return num1+num2
+print(mysum2(200,2000))
+print((lambda num1,num2 : num1+num2)(200,2000))
+
+
+#======map, filter =======
+#map---> map(함수, input 리스트)
+'''l2=[1,2,3,4,5]
+addone()
+l3=[2,3,4,5,6,7]
+map(addone,l2)  #---->l3
+
+#map(lambda함수, input 리스트)
+#map(addone,l2)(lambda x: x+1)(100)
+print(list(map(lambda x :x+1,l2)))
+'''
+
+#lambda n1,n2 : n1+n2
+l1=[1,2,3,4,5,6,7,8,9,10]
+l2=[1,2,3,4,5]
+print(list(map(lambda n1,n2 : n1+n2,l1,l2)))
